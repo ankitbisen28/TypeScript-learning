32781e833228bee3378a47b338e28daf183ba611
Ankit Bisen <ankitbisen288@gmail.com>
2023-11-09 20:21:39 +0530

    array and objects

---
+++ 02Basics/myArray.ts
@@ -0,0 +1,29 @@
+// const superHeros = string[] = [];
+
+// const heroPower = number[] = [];
+
+// const heroPower: Array<number> = [];
+
+// superHeros.push("spiderman")
+// heroPower.push(400);
+
+
+// superHeros.push("spiderman")
+// heroPower.push(2);
+
+
+// type User = {
+//     name: string;
+//     isActive: boolean;
+// };
+
+// const allUsers: User[] = [];
+// allUsers.push({ name: "ankit", isActive: true });
+// allUsers.push({ name: "pranay", isActive: false });
+
+// console.log(allUsers);
+
+// const MLModels: number [][] = [
+//     [255, 255,255],
+//     [255, 255, 255]
+// ]
--- 02Basics/myObjects.js
+++ 02Basics/myObjects.js
@@ -3,29 +3,21 @@
 //   email: "ankitbisen@mail.com",
 //   isActive: true,
 // };
-
 // function createUser({ name: string, isPaid: boolean }) {}
-
 // let newUser = { name: "ankit", isPaid: true, email: "ankitbisen@mail.com" };
-
 // createUser(newUser);
-
 // function createCourse(): { name: string; price: number } {
 //   return { name: "reactjs", price: 388 };
 // }
-
 // type User = {
 //   name: string;
 //   email: string;
 //   isActive: boolean;
 // };
-
 // function createUser(user: User) {
 //   return { name: "", email: "", isActive: "" };
 // }
-
 // createUser({ name: "", email: "", isActive: true });
-
 // type User = {
 //   readonly _id: string; // can't changeable beacuse of read only property
 //   name: string;
@@ -33,24 +25,19 @@
 //   isActive: boolean;
 //   creditCardDetails?: number; // if there is no value it will not throw you error. because we passed ?
 // };
-
 // let myUser: User = {
 //   _id: "34234",
 //   name: "Ankit",
 //   email: "ankitbisen@gmail.com",
 //   isActive: true,
 // };
-
 // myUser._id = "343234"
-
 // type cardNumber = {
 //   cardnumber: number;
 // };
-
 // type cardDate = {
 //   cardDate: number;
 // };
-
 // type cardDetails = cardDate &
 //   cardNumber & {
 //     cvv: number;
