811a69f0ccc464739a56ebcc2634948fc931bf50
Ankit Bisen <ankitbisen288@gmail.com>
2023-11-14 10:38:00 +0530

    class, getter and setter

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -1,2 +1,33 @@
 "use strict";
-console.log("typescript is here onces again");
+// class User {
+//   public email: string;
+//   private name: string;
+//   #name: string = ""; // this is another way to private a variable
+//   city: string = "";
+//   constructor(email: string, name: string) {
+//     (this.email = email), (this.name = name);
+//   }
+// }
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this.city = "";
+        (this.email = email), (this.name = name);
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    set getCourseCount(courseCount) {
+        if (courseCount <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseCount;
+    }
+}
+const Ankit = new User("ankitbisen@mail.com", "Ankit");
+// Ankit.city = 1;
--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -1,1 +1,38 @@
-console.log("typescript is here onces again")
+// class User {
+//   public email: string;
+//   private name: string;
+//   #name: string = ""; // this is another way to private a variable
+//   city: string = "";
+//   constructor(email: string, name: string) {
+//     (this.email = email), (this.name = name);
+//   }
+// }
+
+class User {
+  private _courseCount = 1;
+
+  readonly city: string = "";
+  constructor(public email: string, public name: string) {
+    (this.email = email), (this.name = name);
+  }
+
+  private deleteToken() {
+    console.log("Token deleted");
+  }
+
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  set getCourseCount(courseCount: number) {
+    if (courseCount <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+    this._courseCount = courseCount;
+  }
+}
+
+const Ankit = new User("ankitbisen@mail.com", "Ankit");
+// Ankit.city = 1;
+
+// Ankit.deleteToken()
